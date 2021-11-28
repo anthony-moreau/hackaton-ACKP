1814edf8dbcf03d2541416b8aa5e038cd72c01b9
Chaimae EL HOUJJAJI <90857148+ChaimaeEH@users.noreply.github.com>
2021-11-28 15:52:30 +0100

    Update DESeq2.R

--- DESeq2.R
+++ DESeq2.R
@@ -1,6 +1,11 @@
-# j'ai d'abord téléchargé tout le github hackahton sur mon bureau 
+##############################################################################################
+# Fichier DESeq2 permettant l'obtention de : 
+#     - un fichier DESeq2.Rdata
+#     - un fichier Rplots.pdf avec les graphes que l'on trace dans ce script R 
+##############################################################################################
 
-# téléchargement des données (changer le path vers l'endroit ou se trouve le dossier que vous avez téléchargé )
+
+## téléchargement des données 
 
 SRR628582 <- read.table("result/SRR628582.counts", head=TRUE)
 SRR628583 <- read.table("result/SRR628583.counts", head=TRUE)
@@ -22,94 +27,122 @@ SRR628589 <- read.table("result/SRR628589.counts", head=TRUE)
 
 library("DESeq2")
 
-###### création du jeu de données avec uniquement les counts à la fin puis renommer les colonnes 
+### Création des jeu de données
+
+# On créée dans la variable `data` le jeu de donnée, avec le nom du GeneId en 1ère colonne et les counts des 8 SRR par la suite. 
 
 data <- data.frame(SRR628582$Geneid,SRR628582$mapping.SRR628582.bam,SRR628583$mapping.SRR628583.bam,SRR628584$mapping.SRR628584.bam,SRR628585$mapping.SRR628585.bam,SRR628586$mapping.SRR628586.bam,SRR628587$mapping.SRR628587.bam,SRR628588$mapping.SRR628588.bam,SRR628589$mapping.SRR628589.bam)
-#data <- rename(data, Geneid = SRR628582.Geneid)
-#data <- rename(data, SRR628582 = SRR628582.mapping.SRR628582.bam)
-#data <- rename(data, SRR628583 = SRR628583.mapping.SRR628583.bam)
-#data <- rename(data, SRR628584 = SRR628584.mapping.SRR628584.bam)
-#data <- rename(data, SRR628585 = SRR628585.mapping.SRR628585.bam)
-#data <- rename(data, SRR628586 = SRR628586.mapping.SRR628586.bam)
-#data <- rename(data, SRR628587 = SRR628587.mapping.SRR628587.bam)
-#data <- rename(data, SRR628588 = SRR628588.mapping.SRR628588.bam)
-#data <- rename(data, SRR628589 = SRR628589.mapping.SRR628589.bam)
 
 
-###### Métadonnées: récuperation au format data.frame des labels :
+#On crée les métadonnées: récuperation au format data.frame des labels :
+  
 samples = c("SRR628582", "SRR628583", "SRR628584", "SRR628585", "SRR628586", "SRR628587", "SRR628588", "SRR628589")
-Type_M_WT = c("M", "M", "WT", "WT", "WT", "WT", "WT", "M")
+Type_M_WT = c("M", "M", "WT", "WT", "WT", "WT", "WT", "M") # M: mutants, WT: Wild-Type
 MetaData = data.frame(samples, Type_M_WT)
 
 
-###### Visualiser les données :
+#On visualise les premières lignes de la variable `data`
+
 head(data)
+
+
+#On visualise les données dans la variable `MetaData`
+
 print(MetaData)
 
 
-###### Construction de l'objet DESEQDataSet
+### Construction de l'objet DESEQDataSet
+
 dds <- DESeqDataSetFromMatrix(countData=data, colData=MetaData, design=~Type_M_WT, tidy = TRUE)
 
-###### Regardons à quoi ressemble de dds 
-
+#Regardons à quoi ressemble le `dds` : 
+  
 print(dds)
 
-###### Lancons la fonction DESEQ maintemant 
+### Lançons la fonction DESeq et Regardons la table des résultats 
 
 dds <- DESeq(dds)
-
-###### Regardons la table de résultat
-
 res <- results(dds)
-head(results(dds, tidy=TRUE)) #let's look at the results table
+dds$Type_M_WT <- relevel(dds$Type_M_WT, ref ="WT") # on modifie pour que la reference soit le WT et pas le M (qui est pris automatiquement car alphabétiquement il est plus proche)
+levels(dds$Type_M_WT) # on vérifie que le WT a bien été pris comme référence 
+head(results(dds, tidy=TRUE))
 
-######  Summary de l'expression différentiel des gènes 
+###  Résumé de l'expression différentiel des gènes 
 
-summary(res) #summary of results
+summary(res) 
 
-#####  Trier la liste récapitulative par p-value
+#On souhaite maintenant comparer avec l'article 2. Pour cela on modifie les paramètres, on prend pvalue = 0,05 et LFC=1,5. 
+
+res2 <- results(dds,alpha = 0.05, lfcThreshold = 1.5, pAdjustMethod = 'none') 
+summary(res2)
+
+###  Trier la liste récapitulative par p-value ajusté décroissante
+
+#On affiche la liste des 10 gènes avec les padj les plus faibles donc les 10 gènes avec une Expression Différentielle la plus forte. 
 
 res <- res[order(res$padj),]
-head(res, 11)
+head(res, 10) # pour afficher la liste des 10 gènes avec les padj les plus faibles 
 
-##### On réalise des plotCounts pour comparer les comptes normalisés entre les mutants et WT pour quelques gènes
 
-par(mfrow=c(2,3))  # par(mfrow=c(2,3)) si je fais 6 graphes, ça les classes en 2 lignes 3 colonnes par exemple 
+#Ainsi on a les identifiants des 10 gènes avec une pdjust la plus faibles, essayons de réaliser des graphiques pour mieux visualiser l'expression différentielle de ces gènes. 
 
-# exemple du gène ENSG00000115524 = SF3B1 dont parlent les articles 
+### On réalise des plotCounts pour comparer les comptes normalisés entre les mutants et les WildType pour quelques gènes
 
+#Exemple du gène ENSG00000115524 = SF3B1 dont parlent les articles 
+
+par(mfrow=c(1,1))
 plotCounts(dds, gene="ENSG00000115524", intgroup="Type_M_WT")
 
-# exemple des 9 autres gènes les plus exprimés c'est à dire dont la p-value est la plus faible 
+
+#Exemple des 9 autres gènes les plus exprimés c'est à dire dont la p-value est la plus faible 
+
+par(mfrow=c(3,3))  # par(mfrow=c(2,3)) si je fais 6 graphes, ça les classes en 2 lignes 3 colonnes par exemple 
 
 plotCounts(dds, gene="ENSG00000164659", intgroup="Type_M_WT")
 plotCounts(dds, gene="ENSG00000234261", intgroup="Type_M_WT")
 plotCounts(dds, gene="ENSG00000153283", intgroup="Type_M_WT")
 plotCounts(dds, gene="ENSG00000214212", intgroup="Type_M_WT")
 plotCounts(dds, gene="ENSG00000142621", intgroup="Type_M_WT")
-#plotCounts(dds, gene="ENSG00000186973", intgroup="Type_M_WT")
-#plotCounts(dds, gene="ENSG00000286122", intgroup="Type_M_WT")
-#plotCounts(dds, gene="ENSG00000103449", intgroup="Type_M_WT")
-#plotCounts(dds, gene="ENSG00000007038", intgroup="Type_M_WT")
+plotCounts(dds, gene="ENSG00000186973", intgroup="Type_M_WT")
+plotCounts(dds, gene="ENSG00000286122", intgroup="Type_M_WT")
+plotCounts(dds, gene="ENSG00000103449", intgroup="Type_M_WT")
+plotCounts(dds, gene="ENSG00000007038", intgroup="Type_M_WT")
 
-##### Réalisation du Volcano Plot 
+
+### Réalisation du Volcano Plot 
+
+#On réalise ici un Volcano Plot avec la librairie DESeq2, toutefois, sur le deuxième fichier R, on a tracé une version améliorée, le EnhancedVolcano Plot qui permet d'avoir les étiquettes des gènes sur les points. 
 
 #reset par
-par(mfrow=c(1,1))
+par(mfrow=c(1,2))
+
 # Faire un volcano plot basique 
 with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-10,10)))
 
-# Ajouter des points colorés: bleu si padj<0.01, rouge si log2FC>1 et padj<0.05)
+# Ajouter des points colorés: padj<.01 et LFC = 4
 with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
 with(subset(res, padj<.01 & abs(log2FoldChange)>4), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+abline(v=0, col="black", lty=3, lwd=1.0)
+abline(v=-4, col="black", lty=4, lwd=2.0)
+abline(v=4, col="black", lty=4, lwd=2.0)
+abline(h=-log10(max(res$pvalue[res$padj<.01], na.rm=TRUE)), col="black", lty=4, lwd=2.0)
 
 
-####### ACP avec library DESeq2
+# Faire un volcano plot basique 
+with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-10,10)))
 
-# Nous devons d'abord transformer les données bruts de comptage
-# La fonction vst effectuera une transformation stabilisant la variance.
+# Ajouter des points colorés: padj<.002 et LFC = 2
+with(subset(res, padj<.0002 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
+with(subset(res, padj<.0002 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+abline(v=0, col="black", lty=3, lwd=1.0)
+abline(v=-2, col="black", lty=4, lwd=2.0)
+abline(v=2, col="black", lty=4, lwd=2.0)
+abline(h=-log10(max(res$pvalue[res$padj<.0002], na.rm=TRUE)), col="black", lty=4, lwd=2.0)
+
+
+### ACP avec la librairie DESeq2
+
+#Nous devons d'abord transformer les données bruts de comptage: la fonction vst effectuera une transformation stabilisant la variance.
 
 vsdata <- vst(dds, blind=FALSE)
 plotPCA(vsdata, intgroup="Type_M_WT")
-
-save.image(file="analysis_R/Analysis.Rdata")
